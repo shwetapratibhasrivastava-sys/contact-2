53e31df0562c00deec32480eba32238d8de05668
Shweta Srivastava <shwetapratibhasrivastava@gmail.com>
2026-05-28 14:00:13 +0530

    middleware added

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+const authMiddlware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.json("No token given");
+  }
+
+  const token = authHeader.split(" ")[1];
+  
+  try {
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decode;
+    next();
+  } catch (error) {
+    return res.json(error.message);
+  }
+};
